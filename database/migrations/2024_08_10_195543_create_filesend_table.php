6bdde7208a86572f019c948c57934975e16e94b2
Wendel Ulhoa <wendelulhoa@gmail.com>
2024-08-10 17:06:16 -0300

    master

--- database/migrations/2024_08_10_195543_create_filesend_table.php
+++ database/migrations/2024_08_10_195543_create_filesend_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('path');
             $table->string('type');
+            $table->date('forget_in');
             $table->timestamps();
         });
     }
