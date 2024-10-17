36af76b5a4866e2879e660307439683d8d523849
Wendel Ulhoa <wendelulhoa@gmail.com>
2024-10-17 09:49:24 -0300

    feat: :fire: Adicionado o checkinstances via http ai evita conflitos de erros de terminais

--- app/Console/Commands/CheckInstancesCommand.php
+++ app/Console/Commands/CheckInstancesCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Http\Controllers\Puppeter\Puppeteer;
 use App\Models\Instance;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class CheckInstancesCommand extends Command
@@ -29,59 +30,14 @@ class CheckInstancesCommand extends Command
     public function handle()
     {
         // Verifica se o processo está ativo
-        $directory = public_path('chrome-sessions');
-        $instances = Instance::where(['connected' => true])->pluck('session_id')->toArray();
+        $instances = Instance::where(['connected' => true])->get();
 
-        foreach ($instances as $sessionId) {
-            // Extrai o nome da sessão
-            $pidFile = "$directory/$sessionId/pids/chrome-$sessionId.pid";
-
-            // Verifica se o arquivo PID existe
-            if (file_exists($pidFile)) {
-                $pid = file_get_contents($pidFile);
-
-                // Verifica se o processo está ativo e sobe a instância
-                if (!$this->isProcessRunning($pid)) {
-                    try {
-                        // Sobe a instância
-                        $page = (new Puppeteer)->init($sessionId, 'https://web.whatsapp.com', view('whatsapp-functions.injected-functions-minified')->render(), 'window.WUAPI');
-                        
-                        // Verifica a conexão
-                        $content = $page->evaluate("window.WUAPI.startSession();")['result']['result']['value'];
-                        
-                        // Atualiza o status da instância
-                        $active = ($content['success'] ?? false) ? 'ativo' : 'inativo';
-
-                        // Loga a ação
-                        Log::channel('daily')->info("Processo com PID $pid ($sessionId) está $active.");
-                        sleep(2);
-                    } catch (\Throwable $th) {
-                        Log::error("Erro ao verificar a instância $sessionId: " . $th->getMessage());
-                    }
-                }
-            } else {
-                Log::warning("Arquivo PID não encontrado para a sessão $sessionId.");
-            }
+        // Sobe as instâncias via url com o checkconnection
+        foreach ($instances as $instance) {
+            Http::withHeaders([
+                'Authorization' => env('API_KEY'),
+                'api-key' => $instance->token
+            ])->get(route('wapiwu.checkconnection', ['sessionId' => $instance->session_id]));
         }
-    }
-
-    /**
-     * Verifica se o processo está ativo no sistema
-     *
-     * @param int $pid
-     * @return bool
-     */
-    private function isProcessRunning(int $pid): bool
-    {
-        // Verifica se o PID é válido (maior que zero)
-        if ($pid <= 0) {
-            return false;
-        }
-
-        // Executa o comando para verificar o processo
-        $result = shell_exec(sprintf('ps -p %d 2>&1', $pid));
-
-        // Verifica se a saída contém o PID e não retorna erro
-        return (strpos($result, (string) $pid) !== false);
     }
 }
