65dd48842b4e2d034fc49a97ef98d5b99b169e5b
Wendel Ulhoa <wendelulhoa@gmail.com>
2024-10-26 12:09:42 -0300

    master

--- app/Http/Controllers/Puppeter/Browser.php
+++ app/Http/Controllers/Puppeter/Browser.php
@@ -206,7 +206,7 @@ Class Browser {
 
             // Comando para iniciar o navegador
             $command = "
-               cpulimit -l 600 Xvfb :$port -screen $port 800x600x24 & DISPLAY=:$port
+               Xvfb :$port -screen $port 800x600x24 & DISPLAY=:$port
                google-chrome --headless \
                 --disable-gpu \
                 --disable-cache \
