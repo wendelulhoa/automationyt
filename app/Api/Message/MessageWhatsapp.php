d02b085251a2483b3b55c566141327b56cd26089
root <root@vmi914461.contaboserver.net>
2024-11-08 20:05:17 +0100

    beta-01

--- app/Api/Message/MessageWhatsapp.php
+++ app/Api/Message/MessageWhatsapp.php
@@ -10,9 +10,9 @@ class MessageWhatsapp {
     use UtilWhatsapp;
 
     /**
-     * Tempo de espera 1s
+     * Tempo de espera 3s
      */
-    private $sleepTime = 2;
+    private $sleepTime = 3;
 
     /**
      * Envia uma mensagem de texto
