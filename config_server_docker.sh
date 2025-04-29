1b2849e491a17d297c2047f701ef09a06e9f0a97
Matheus de Araújo Oliveira <matheus.encode@gmail.com>
2025-04-29 12:52:26 -0300

    feat: Adiciona scripts automatizados para instalação makeFile e dev para auxílio no desenvolvimento

--- config_server_docker.sh
+++ config_server_docker.sh
@@ -1,24 +1,18 @@
 # Obtém o caminho do diretório das sessões do chrome
 CHROME_PATH=$(dirname $(pwd))
 
-# Cria o volume de storage
-# docker volume create storage-wuapi
+# Instala o node e npm
+sudo apt install nodejs npm -y
 
-# # Cria uma nova rede da wuapi
-# docker network create redewuapi
+# Configura o js do server de controle de instâncias
+cd scripts && npm install
 
-# #  Instala o node e npm
-# sudo apt install nodejs npm -y
+# Configura o pm2 para rodar o server de controle de instâncias
+sudo npm install -g pm2
+pm2 stop instance-controller
+pm2 delete instance-controller
+pm2 start server.js --name instance-controller
 
-# # Configura o js do server de controle de instâncias
-# cd scripts && npm install
-
-# # Configura o pm2 para rodar o server de controle de instâncias
-# sudo npm install -g pm2
-# pm2 stop instance-controller
-# pm2 delete instance-controller
-# pm2 start server.js --name instance-controller
-
-# # Cria uma nova pasta de instâncias
-# mkdir -p $CHROME_PATH/chrome-sessions
-# chmod -R 777 $CHROME_PATH/chrome-sessions
+# Cria uma nova pasta de instâncias
+mkdir -p $CHROME_PATH/chrome-sessions
+chmod -R 777 $CHROME_PATH/chrome-sessions
